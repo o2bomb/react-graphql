af46a448c39c25c5e41552951f1445ea601f1554
felix t <41817193+o2bomb@users.noreply.github.com>
2021-01-25 18:52:05 +0800

    Add boilerplate code for GraphQL server

---
+++ server/env.d.ts
@@ -0,0 +1,11 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    POSTGRES_PASSWORD: string;
+    DATABASE_NAME: string;
+    DATABASE_URL: string;
+    REDIS_URL: string;
+    CORS_ORIGIN: string;
+    PORT: string;
+    SESSION_SECRET: string;
+  }
+}
